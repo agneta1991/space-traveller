6d8399df9c9d8e9f75b22873c0bb64da7eb5d5dd
agneta1991 <pupienytea@gmail.com>
2023-08-29 12:52:06 +0300

    set up basics for profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,3 +1,16 @@
-const Profile = () => <h1>Profile</h1>;
+import '../styles/profile.css';
+
+const Profile = () => (
+  <div className="main-profile-div">
+    <div className="missions-profile-div">
+      <h2>My Missions</h2>
+      <div className="content">Content</div>
+    </div>
+    <div className="rockets-profile-div">
+      <h2>My Rockets</h2>
+      <div className="content">Content</div>
+    </div>
+  </div>
+);
 
 export default Profile;
