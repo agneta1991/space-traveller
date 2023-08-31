84df5c87593f6467bf06c1483f676a7f0a1b6ae0
Ochiengsteven <stevenochieng6305@gmail.com>
2023-08-31 21:40:00 +0300

    *

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import missionsReducer from './missions/missionsSlice';
 import rocketSliceReducer from './rockets/rocketSlice';
 
 const store = configureStore({
+
   reducer: {
     rockets: rocketSliceReducer,
     missions: missionsReducer,
